439ea55c1b76a929569792293eff314fb50067f3
Natalie Smith <nathesloth@outlook.com>
2024-05-26 15:56:42 +1000

    Testing

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import BrowserRouter from "react-router-dom/BrowserRouter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
