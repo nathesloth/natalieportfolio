7cf10800cb4b31d142914c689490abe8dba0bb81
Natalie Smith <nathesloth@outlook.com>
2024-05-26 14:51:16 +1000

    tset

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import BrowserRouter from "react-router-dom/BrowserRouter";
 import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>
 );
+
+reportWebVitals();
