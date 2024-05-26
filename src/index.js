134ca11d904bd92ab1a92fa295ac4d3120632ec3
Natalie Smith <nathesloth@outlook.com>
2024-05-26 14:48:08 +1000

    more changes

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import { BrowserRouter } from "react-router-dom";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import BrowserRouter from "react-router-dom/BrowserRouter";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
