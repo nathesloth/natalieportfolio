50a53b9dd85d251885431704caf3fb2a68d1a7e1
Natalie Smith <nathesloth@outlook.com>
2024-05-26 15:09:05 +1000

    html

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
-
-reportWebVitals();
