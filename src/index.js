6f778f0189d19673dba6685e69be92910465e7c8
Natalie Smith <nathesloth@outlook.com>
2024-05-26 16:00:45 +1000

    change rerender

--- src/index.js
+++ src/index.js
@@ -5,11 +5,5 @@ import { BrowserRouter } from "react-router-dom";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<App />);
